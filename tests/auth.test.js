41ae73ba35a81d6f7e843b2d98afd94139a0d5b0
arvind <apple@apples-MacBook-Pro.local>
2022-10-20 12:34:27 +0530

    signup test cases completed

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -20,10 +20,78 @@ describe('test cases for signup api', ()=>{
         });
     })
 
+
+    test('return 400 bad when user skip one or more mandatory fields', ()=>{
+        return request(app)
+        .post('/auth/signup')
+        .send({
+            name: "arvind",
+            password: "arvind@123"
+        })
+        .expect(400)
+        .then((response)=>{
+            expect({
+                message: 'please fill the all  required fields'
+            })
+        });
+    });
+
+
+
+    test('return 404 bad when  user send invailid url', ()=>{
+        return request(app)
+        .post('/auth/signu')
+        .send({
+            name: "arvind",
+            email: "arvind@gmail.com",
+            password: "arvind@123"
+        })
+        .expect(404)
+        .then((response)=>{
+            expect({
+                message: 'page not found'
+            })
+        });
+    });
+
+
+
+    test('return 409 conflict when  user send duplicate email', ()=>{
+        return request(app)
+        .post('/auth/signup')
+        .send({
+            name: "arvind",
+            email: "arvind@gmail.com",
+            password: "arvind@123"
+        })
+        .expect(409)
+        .then((response)=>{
+            expect({
+                message: 'email is already exist'
+            })
+        });
+    });
+
+
+    test('return 422 Unprocessable Entity when user enter number in name or email field', ()=>{
+        return request(app)
+        .post('/auth/signup')
+        .send({
+            name: "1234345678",
+            email: "arvind@gmail.com",
+            password: "arvind@123"
+        })
+        .expect(422)
+        .then((response)=>{
+            expect({
+                message: 'please enter correct value '
+            })
+        });
+    });
+
+
 });
 
-
-// test cases for login api 
 
 
 
