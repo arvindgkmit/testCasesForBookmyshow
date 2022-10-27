50d2bdf6c33d7b0c3a725f5456f39c7c45f90c58
arvind <apple@apples-MacBook-Pro.local>
2022-10-27 11:32:58 +0530

    test cases for add shows api  is completed

--- tests/auditorium.test.js
+++ tests/auditorium.test.js
@@ -84,3 +84,89 @@ test('return 401 unauthorized when user not login', ()=>{
 
 
  });
+
+
+
+//  test cases for add shows 
+describe('test cases for add shows api', ()=>{
+
+    test('return 201 ok when admin add a auditorium', ()=>{
+        return  request(app)
+        .post('/auditorium/shows',)
+        .send({
+            movieId: 7,
+            auditoriumId: 3,
+            dateTime: "20-10-2022 12:00:00"
+        })
+        .expect(201)
+        .expect(req.headers).toEqual({
+            Authorization: 'eyJhbGciOiJIUzI1NiJ9.Mw.7lgYa5GpLnFasIFD8Leet3arTtYx27Df8Gau6dhIEYg'
+        })
+        .then((response)=>{
+            expect(response.body).toEqual({
+                message: 'show added successfully'
+            })
+        });
+    });
+
+
+    test('return 400 bad request when admin add skip any field', ()=>{
+        return  request(app)
+        .post('/auditorium/shows',)
+        .send({
+            movieId: 7,
+            auditoriumId: 3,
+            dateTime: "20-10-2022 12:00:00"
+        })
+        .expect(400)
+        .expect(req.headers).toEqual({
+            Authorization: 'eyJhbGciOiJIUzI1NiJ9.Mw.7lgYa5GpLnFasIFD8Leet3arTtYx27Df8Gau6dhIEYg' 
+        })
+        .then((response)=>{
+            expect(response.body).toEqual({
+                message: 'please fill the all required fields'
+            })
+        });
+    });
+
+    test('return 403 forbidden when admin send wrong token', ()=>{
+        return  request(app)
+        .post('/auditorium/shows')
+        .send({
+            movieId: 7,
+            auditoriumId: 3,
+            dateTime: "20-10-2022 12:00:00"
+        })
+        .expect(403)
+        .expect(req.headers).toEqual({
+            Authorization: 'eyJhbGciOiJIUzI1NiJ9.Mw.7lgYa5GpLnFasIFD8Leet3arTtYx27Df8Gau6dhIEYg'
+        })
+        .then((response)=>{
+            expect(response.body).toEqual({
+                message: 'you can not access this page'
+            })
+        });
+    });
+
+
+test('return 401 unauthorized when user not login', ()=>{
+    return request(app)
+    .post('/auditorium/shows')
+    .send({
+        movieId: 7,
+        auditoriumId: 3,
+        dateTime: "20-10-2022 12:00:00"
+    })
+    .expect(401)
+    .expect(req.headers).toEqual({
+        Authorization: '' 
+    })
+    .then((response)=>{
+        expect(response.body).toEqual({
+            message: 'please login'
+        });
+});
+});
+
+
+ });
